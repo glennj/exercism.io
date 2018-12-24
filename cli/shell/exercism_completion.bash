d2b1e4b8ebe6f2895d993778d6eb359afef019b0
Glenn Jackman <jackman@pythian.com>
2018-12-24 11:12:53 -0500

    changes

---
+++ cli/shell/exercism_completion.bash
@@ -0,0 +1,51 @@
+_exercism () {
+  local cur prev
+
+  COMPREPLY=()   # Array variable storing the possible completions.
+  cur=${COMP_WORDS[COMP_CWORD]}
+  prev=${COMP_WORDS[COMP_CWORD-1]}
+  opts="--verbose --timeout"
+
+  commands="configure download open
+  submit troubleshoot upgrade version workspace help"
+  config_opts="--show"
+  version_opts="--latest"
+
+  if [ "${#COMP_WORDS[@]}" -eq 2 ]; then
+    case "${cur}" in
+      -*)
+        COMPREPLY=( $( compgen -W  "${opts}" -- "${cur}" ) )
+        return 0
+        ;;
+      *)
+        COMPREPLY=( $( compgen -W  "${commands}" "${cur}" ) )
+        return 0
+        ;;
+    esac
+  fi
+
+  if [ "${#COMP_WORDS[@]}" -eq 3 ]; then
+    case "${prev}" in
+      configure)
+        COMPREPLY=( $( compgen -W "${config_opts}" -- "${cur}" ) )
+        return 0
+        ;;
+      version)
+        COMPREPLY=( $( compgen -W "${version_opts}" -- "${cur}" ) )
+        return 0
+        ;;
+      help)
+        COMPREPLY=( $( compgen -W "${commands}" "${cur}" ) )
+        return 0
+        ;;
+      *)
+        return 0
+        ;;
+    esac
+  fi
+
+  return 0
+}
+
+complete -o bashdefault -o default -o nospace -F _exercism exercism 2>/dev/null \
+	|| complete -o default -o nospace -F _exercism exercism
---
+++ ruby/scale-generator/scale_generator.rb
@@ -0,0 +1,57 @@
+class Scale
+  CHROMATIC = {
+    sharps: %w[A A# B C C# D D# E F F# G G#],
+    flats:  %w[A Bb B C Db D Eb E F Gb G Ab]
+  }.freeze
+
+  FLATS = %w[F Bb Eb Ab Db Gb d g c f bb eb].freeze
+
+  SCALE_INTERVALS = {
+    chromatic: '',         major:      'MMmMMMm', minor:      'MmMMmMM',
+    dorian:    'MmMMMmM',  mixolydian: 'MMmMMmM', lydian:     'MMMmMMm',
+    phrygian:  'mMMMmMM',  locrian:    'mMMmMMM', enigma:     'mAMMMmM',
+    octatonic: 'MmMmMmMm', hexatonic:  'MMMMMM',  pentatonic: 'MMAMA',
+    harmonic_minor: 'MmMMmAm'
+  }.freeze
+
+  INTERVAL = { 'm' => 1, 'M' => 2, 'A' => 3 }.freeze
+
+  attr_reader :name, :pitches
+
+  def initialize(note, type, intervals = '')
+    raise ArgumentError, 'Unknown type' unless SCALE_INTERVALS.key? type
+    raise ArgumentError, 'Invalid intervals' if SCALE_INTERVALS[type] != intervals
+
+    tonic = note.capitalize
+    raise ArgumentError, "Unknown tonic #{tonic}" \
+      unless %i[sharps flats].any? { |s| CHROMATIC[s].include?(tonic) }
+
+    @name = "#{tonic} #{type}"
+
+    @pitches = filter(rotate(notes(note), tonic), intervals)
+  end
+
+  private
+
+  def notes(note)
+    CHROMATIC[FLATS.include?(note) ? :flats : :sharps]
+  end
+
+  def rotate(notes, tonic)
+    idx = notes.index tonic
+    # a 2-dot range and a 3-dot range
+    notes[idx..-1] + notes[0...idx]
+  end
+
+  def filter(notes, intervals)
+    return notes if intervals.empty?
+    
+    pitches = []
+    idx = 0
+    intervals.chars.each do |i|
+      pitches << notes[idx]
+      idx += INTERVAL[i]
+    end
+    pitches
+  end
+end
