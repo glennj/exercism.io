b51ec7b565a211f299648199c5df06263449b681
Glenn Jackman <jackman@pythian.com>
2019-04-07 09:53:01 -0400

    python

--- python/saddle-points/saddle_points.py
+++ python/saddle-points/saddle_points.py
@@ -2,12 +2,12 @@ def saddle_points(matrix):
     if any(len(row) != len(matrix[0]) for row in matrix):
         raise ValueError('Irregular matrix.')
 
-    rows = matrix
-    cols = list(zip(*matrix))
+    row_maxima = list(map(max, matrix))
+    col_minima = list(map(min, list(zip(*matrix))))
+
     return {
         (r+1, c+1)
-        for r in range(len(rows))
-        for c in range(len(cols))
-        if rows[r][c] == max(rows[r]) and
-           rows[r][c] == min(cols[c])
+        for r, row_max in enumerate(row_maxima)
+        for c, col_min in enumerate(col_minima)
+        if row_max == col_min
     }
