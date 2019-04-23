08e5b6566055b5115c11f7847a6be95643cadfe4
Glenn Jackman <jackman@pythian.com>
2019-04-23 15:32:10 -0400

    R:comments

--- r/raindrops/raindrops.R
+++ r/raindrops/raindrops.R
@@ -23,8 +23,9 @@ raindrops <- function(number) {
 
 # community
 #
-# amazingly thorough
+# amazingly thorough:
 # https://exercism.io/tracks/r/exercises/raindrops/solutions/4e5858d9e31b4b0bb374cec512ad265f
+#
 #
 # with vectors
 # https://exercism.io/tracks/r/exercises/raindrops/solutions/57060f71368c409a9c77530ec4f80a18
@@ -36,4 +37,20 @@ raindrops <- function(number) {
 #     } else {
 #       as.character(number)
 #     }
+#   }
+#
+#
+# similar
+# https://exercism.io/tracks/r/exercises/raindrops/solutions/cb33cb138d4848bd860469aad324649c
+#
+#   raindrops <- function(number) {
+#     factors <- c(3, 5, 7)
+#     sounds <- c("Pling", "Plang", "Plong")
+#     
+#     factors_present <- number %% factors == 0
+#     
+#     if(any(factors_present))
+#       return(paste0(sounds[factors_present], collapse = ""))
+#     else
+#       return(as.character(number))
 #   }
