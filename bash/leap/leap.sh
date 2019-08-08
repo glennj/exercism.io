deb43f789c0ccb58def2034ca945829bdc51a449
Glenn Jackman <jackman@pythian.com>
2019-08-08 11:34:58 -0400

    more efficient tests

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-shopt -s extglob
 
-if [[ $# -ne 1 || $1 != +([[:digit:]]) ]]; then
+if [[ $# -ne 1 || $1 == *[^[:digit:]]* ]]; then
     echo "Usage: $(basename "$0") <year>" >&2
     exit 1
 fi
 
-if (( $1 % 400 == 0 || ($1 % 4 == 0 && $1 % 100 != 0) )); then
+#if (( $1 % 400 == 0 || ($1 % 4 == 0 && $1 % 100 != 0) )); then
+
+# below is a more efficient sequence of tests -- filter out
+# non-leap years faster
+if (( $1 % 4 == 0 && ($1 % 100 != 0 || $1 % 400 == 0) )); then
     echo true
 else
     echo false
