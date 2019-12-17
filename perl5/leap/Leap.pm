476b6d17938e5c7f5f856dddfab4752c1add8927
Glenn Jackman <glenn.jackman@gmail.com>
2019-12-17 16:11:55 -0500

    leap: do the 4 check first

--- perl5/leap/Leap.pm
+++ perl5/leap/Leap.pm
@@ -6,7 +6,7 @@ our @EXPORT_OK = qw(is_leap_year);
 
 sub is_leap_year {
     my ($year) = @_;
-    return $year % 400 == 0 || ($year % 4 == 0 && $year % 100 != 0);
+    return $year % 4 == 0 && ($year % 100 != 0 || $year % 400 == 0);
 }
 
 1;
