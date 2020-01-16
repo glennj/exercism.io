f90c985561c52e92aec0b2e8d8a0b4549e2d3d63
Glenn Jackman <glenn.jackman@gmail.com>
2020-01-16 17:00:59 -0500

    bash grains: do wide int addition with plain bash

--- bash/grains/grains.sh
+++ bash/grains/grains.sh
@@ -1,35 +1,64 @@
 #!/bin/bash
-# bash implementation fails due to lack of big integer suuport
 #
-# external tools used: bc
-
-pow2() { echo "2 ^ ($1 - 1)" | bc; }
+# with no external tools used
 
 shopt -s extglob
 
-case $1 in
-    ?(-)+([0-9]) )
-        if (($1 <= 0 || $1 > 64)); then 
-            echo "Error: invalid input" >&2
-            exit 1
-        fi
-        pow2 $1
-        ;;
-    total)
-        # this implementation, while nice and DRY,
-        # is calling bc 65 times:
-        #for i in {1..64}; do
-        #    pow2 $i
-        #done | paste -sd + | bc
+main() {
+    local -a square
+    local result=0
 
-        expression="0"
-        for i in {1..64}; do
-            expression+=" + (2^($i-1))"
-        done
-        echo "$expression" | bc
+    case $1 in
+        [1-9]*([0-9]))
+            (($1 <= 64)) || die "invalid input"
+            calcSquares $1
+            echo ${square[$1]}
+            ;;
+        total)
+            calcSquares 64
+            local total=0
+            for s in "${square[@]}"; do
+                total=$(add $total $s)
+            done
+            echo $total
+            ;;
+        *)
+            die "invalid input"
+            ;;
+    esac
+}
 
-        ;;
-    *)  echo "Error: invalid input" >&2
-        exit 1
-        ;;
-esac
+die() { echo "Error: $*" >&2; exit 1; }
+
+# populate the grains in the necessary squares
+calcSquares() {
+    square=(0 1)
+    for (( i = 2; i <= $1; i++ )); do
+        square[i]=$(add ${square[i-1]} ${square[i-1]})
+    done
+}
+
+# Add two arbitrarily large integers.
+# We'll use string manipulation to avoid integer overflow.
+add() {
+    local a=$1 b=$2 c
+    local result="" carry=0
+
+    # left pad the numbers with zeroes so they're the same width
+    local width
+    (( ${#a} > ${#b} )) && width=${#a} || width=${#b}
+    printf -v a '%0*s' $width $a
+    printf -v b '%0*s' $width $b
+
+    # add the digits from right to left
+    for (( i = width-1; i >= 0; i-- )); do
+        (( c = carry + ${a:i:1} + ${b:i:1} ))
+        result=$((c % 10))${result}
+        (( carry = c / 10 ))
+    done
+    result=${carry}${result}
+
+    echo ${result##+(0)}
+}
+
+main "$@"
