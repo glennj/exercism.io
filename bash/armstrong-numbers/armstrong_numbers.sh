a8783907edd5d32f9fec50e7901b7321f7d4b89f
Glenn Jackman <glenn.jackman@gmail.com>
2020-12-21 11:16:55 -0500

    latest bash tweaks

--- bash/armstrong-numbers/armstrong_numbers.sh
+++ bash/armstrong-numbers/armstrong_numbers.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 is_armstrong() {
     local -i num=$1
@@ -12,3 +12,14 @@ is_armstrong() {
 }
 
 is_armstrong "$1" && echo true || echo false
+exit
+
+
+# golfed wrt LOC:
+for (( len=${#1}, i=0; i<len; i++ )); do 
+    (( sum += ${1:i:1} ** len ))
+done
+(( sum == $1 )) && echo true || echo false
+
+
+
--- bash/atbash-cipher/atbash_cipher.sh
+++ bash/atbash-cipher/atbash_cipher.sh
@@ -3,74 +3,97 @@
 source ../lib/utils.bash
 checkBashVersion 4.0 "associative arrays"
 
-# the alphabet: a bit convoluted, and hardcoding it would
-# be simple, but let's demonstrate a dynamic method.
-alpha=$( set -- {a..z}; IFS=""; echo "$*" )
-
-declare -A map
-for (( i=0, j=${#alpha}-1; i < ${#alpha}; i++, j-- )); do
-    map[${alpha:i:1}]=${alpha:j:1}
-done
-
-
-main() {
-    if (( $# != 2 )); then
-        echo "usage: $(basename "$0") <encode|decode> string" >&2
-        exit 1
-    fi
-
-    case "$1" in
-        encode|decode) "$1" "$2" ;;
-        *) echo "error: unknown subcommand '$1'" >&2; exit 1 ;;
-    esac
-}
-
-encode() { x_code "$1" | groups; }
+############################################################
+encode() { x_code "$1" | grouped; }
 decode() { x_code "$1"; }
-x_code() { echo "$1" | lower_case | alnum_only | encipher; }
+x_code() { alnum_only <<<"$1" | lower_case | encipher; }
 
+############################################################
 # These are nice one line functions that I would use in
-# a production script. Let's try them with plain bash.
+# a production script.
 
-## lower_case() { tr '[:upper:]' '[:lower:]'; }
-## alnum_only() { tr -dc "$alpha[:digit:]"; }
-## encipher()   { tr "$alpha" "$(rev <<< "$alpha")"; }
-## groups()     { sed -Ee "s/.{${1:-5}}/& /g" -e 's/ $//'; }
+#    lower_case() { tr '[:upper:]' '[:lower:]'; }
+#    alnum_only() { tr -dc '[:alnum:]'; }
+#    encipher()   { tr '[:lower:]' 'zyxwvutsrqponmlkjihgfedcba'; }
+#    groups()     { sed -Ee "s/.{${1:-5}}/& /g" -e 's/ $//'; }
+
+# Since I'm emulating external utilities in plain bash,
+# allow these functions to iterate over all the lines that
+# are fed to them.
+
+# The default varname for `read` is `$REPLY`
 
 lower_case() {
     while IFS= read -r; do
-        echo "${REPLY,,}"
+        printf '%s\n' "${REPLY,,}"
     done
 }
 
 alnum_only() {
     while IFS= read -r; do
-        echo "${REPLY//[^[:alnum:]]/}"
+        printf '%s\n' "${REPLY//[^[:alnum:]]/}"
     done
 }
 
 encipher() {
     local result char
+    local -A map
+    createMap
+
     while IFS= read -r; do
-        result="" 
-        for (( i=0; i < ${#REPLY}; i++ )); do
-            char=${REPLY:i:1}
+        result=""
+
+        # a while-read loop to access the characters
+        # of a string is faster than a for loop.
+        while IFS= read -r -d "" -n1 -u3 char; do
             result+=${map[$char]:-$char}
-        done
-        echo "$result" 
+        done 3<<< "$REPLY"
+
+        printf '%s\n' "$result"
     done
 }
 
-groups() {
-    local -i size=${1:-5} i
-    local -a groups
+createMap() {
+    # Create the encryption mapping.
+    # I normally avoid external tools in this track,
+    # but this is rather pretty:
+    local a b
+    while read -r a b; do map[$a]=$b; done < <(
+        paste <(printf '%s\n' {a..z}) \
+              <(printf '%s\n' {z..a})
+    )
+    
+    # in plain bash:
+    #    local -a letters=({a..z})
+    #    local -i len=${#letters[@]} i
+    #    for (( i=0; i<len; i++ )); do
+    #        map[${letters[i]}]=${letters[len - i - 1]}
+    #    done
+}
+
+# This function takes an _optional_ argument:
+# shellcheck disable=SC2120
+grouped() {
+    local -i size=${1:-5} len i
+    local grouped
     while IFS= read -r; do
-        groups=()
-        for (( i=0; i < ${#REPLY}; i += size )); do
-            groups+=( "${REPLY:i:size}" )
+        len=${#REPLY}
+        grouped=${REPLY:0:size}
+        for (( i=size; i < len; i += size )); do
+            grouped+=" ${REPLY:i:size}"
         done
-        echo "${groups[*]}"
+        printf '%s\n' "$grouped"
     done
+}
+
+############################################################
+main() {
+    assert "$# == 2" "usage: ${0##*/} <encode|decode> string"
+
+    case "$1" in
+        encode|decode) "$1" "$2" ;;
+        *) die "error: unknown subcommand '$1'" ;;
+    esac
 }
 
 main "$@"
