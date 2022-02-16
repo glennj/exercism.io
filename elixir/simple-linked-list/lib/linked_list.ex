8ead6524e9f49e160980ca85711183eab5cabd99
Glenn Jackman <glenn.jackman@gmail.com>
2022-02-15 22:16:37 -0500

    elixir linked list

--- elixir/simple-linked-list/lib/linked_list.ex
+++ elixir/simple-linked-list/lib/linked_list.ex
@@ -1,69 +1,89 @@
+# This is not a tuple-based solution: it is quite elegant to
+# use a tuple to represent the list, but I find this solution
+# quite interesting.
+#
+# I'm particularly happy that I've avoided using the Enum
+# module at all.
+
 defmodule LinkedList do
-  @opaque t :: tuple()
+  defstruct [:value, :next, count: 0]
+
+  # A guard macro: determine if the list is empty.
+  defmacrop is_empty(list) do
+    quote do
+      is_nil(unquote(list).next)
+    end
+  end
+
+  @opaque t :: %{value: any(), next: t() | nil, count: non_neg_integer()}
 
   @doc """
   Construct a new LinkedList
   """
   @spec new() :: t
   def new() do
-    {}
+    %__MODULE__{}
   end
 
   @doc """
   Push an item onto a LinkedList
-
+ 
   The list's head holds the most recently added element
   """
   @spec push(t, any()) :: t
   def push(list, elem) do
-    {elem, list}
+    %__MODULE__{value: elem, next: list, count: list.count + 1}
   end
 
   @doc """
   Counts the number of elements in a LinkedList
+ 
+  We _can_ do the count by walking the list, which would be O(N).
+  But by keeping the :count in the struct, and incrementing
+  it when we push(), we have an O(1) operation.
   """
   @spec count(t) :: non_neg_integer()
-  def count(list), do: do_count(list, 0)
+  def count(list), do: list.count
 
-  defp do_count({}, c), do: c
-  defp do_count({_, tail}, c), do: do_count(tail, c + 1)
+  ## recursively:
+  # def count(list), do: do_count(list, 0)
+  # defp do_count(list, c) when is_empty(list), do: c
+  # defp do_count(list, c), do: do_count(list.next, c + 1)
 
   @doc """
   Determine if a LinkedList is empty
   """
   @spec empty?(t) :: boolean()
-  def empty?({}), do: true
-  def empty?(_), do: false
+  def empty?(list), do: is_empty(list)
 
   @doc """
   Get the value of a head of the LinkedList
   """
   @spec peek(t) :: {:ok, any()} | {:error, :empty_list}
-  def peek({}), do: {:error, :empty_list}
-  def peek({value, _}), do: {:ok, value}
+  def peek(list) when is_empty(list), do: {:error, :empty_list}
+  def peek(list), do: {:ok, list.value}
 
   @doc """
   Get tail of a LinkedList
   """
   @spec tail(t) :: {:ok, t} | {:error, :empty_list}
-  def tail({}), do: {:error, :empty_list}
-  def tail({_, tail}), do: {:ok, tail}
+  def tail(list) when is_empty(list), do: {:error, :empty_list}
+  def tail(list), do: {:ok, list.next}
 
   @doc """
   Remove the head from a LinkedList
   """
   @spec pop(t) :: {:ok, any(), t} | {:error, :empty_list}
-  def pop({}), do: {:error, :empty_list}
-  def pop({value, tail}), do: {:ok, value, tail}
+  def pop(list) when is_empty(list), do: {:error, :empty_list}
+  def pop(list), do: {:ok, list.value, list.next}
 
   @doc """
   Construct a LinkedList from a stdlib List
-
+ 
   Recursive, but not tail recursive.
   """
   @spec from_list(list()) :: t
   def from_list([]), do: new()
-
   def from_list([elem | elems]) do
     list = from_list(elems)
     push(list, elem)
@@ -71,15 +91,14 @@ defmodule LinkedList do
 
   @doc """
   Construct a stdlib List LinkedList from a LinkedList
-
+ 
   Recursive, but not tail recursive.
   """
   @spec to_list(t) :: list()
-  def to_list({}), do: []
-
-  def to_list({value, tail}) do
-    prev = to_list(tail)
-    [value | prev]
+  def to_list(list) when is_empty(list), do: []
+  def to_list(list) do
+    prev = to_list(list.next)
+    [list.value | prev]
   end
 
   @doc """
@@ -88,9 +107,11 @@ defmodule LinkedList do
   @spec reverse(t) :: t
   def reverse(list), do: do_reverse(list, new())
 
-  defp do_reverse({}, reversed), do: reversed
+  defp do_reverse(list, reversed) when is_empty(list), do: reversed
 
-  defp do_reverse({value, tail}, reversed) do
-    do_reverse(tail, push(reversed, value))
+  defp do_reverse(list, reversed) do
+    do_reverse(list.next, push(reversed, list.value))
   end
 end
+
+# vim: set ft=elixir:
