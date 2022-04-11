404b269272993be0f62fd102ec89613c6ea5a5cf
Glenn Jackman <jackman@pythian.com>
2022-04-11 15:03:54 -0400

    elixir freelancer-rates

--- elixir/freelancer-rates/lib/freelancer_rates.ex
+++ elixir/freelancer-rates/lib/freelancer_rates.ex
@@ -1,9 +1,9 @@
 defmodule FreelancerRates do
-  defp hours_per_day(), do: 8
-  defp days_per_month(), do: 22
+  @hours_per_day 8
+  @days_per_month 22
 
   def daily_rate(hourly_rate) do
-    1.0 * hourly_rate * hours_per_day()
+    1.0 * hourly_rate * @hours_per_day
   end
 
   def apply_discount(before_discount, discount) do
@@ -11,13 +11,13 @@ defmodule FreelancerRates do
   end
 
   def monthly_rate(hourly_rate, discount) do
-    apply_discount(daily_rate(hourly_rate) * days_per_month(), discount)
+    apply_discount(daily_rate(hourly_rate) * @days_per_month, discount)
     |> Float.ceil
     |> trunc
   end
 
   def days_in_budget(budget, hourly_rate, discount) do
-    budget / monthly_rate(hourly_rate, discount) * days_per_month()
+    budget / monthly_rate(hourly_rate, discount) * @days_per_month
     |> Float.floor(1)
   end
 end
