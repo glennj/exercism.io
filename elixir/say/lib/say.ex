867f0573c9a369da14826f3755306ab428039a17
Glenn Jackman <jackman@pythian.com>
2022-04-27 17:36:24 -0400

    elixir say refactoring

--- elixir/say/lib/say.ex
+++ elixir/say/lib/say.ex
@@ -5,7 +5,7 @@ defmodule Say do
     "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"
   }
 
-  # values will be retrieved by index, hence the nils
+  # values will be retrieved by index (9 => "ninety"), hence the nils
   @tens {
     nil, nil, "twenty", "thirty", "forty", "fifty",
     "sixty", "seventy", "eighty", "ninety"
@@ -14,44 +14,28 @@ defmodule Say do
   @doc """
   Translate a positive integer into English.
   """
-  @spec in_english(integer) :: {atom, String.t()}
-  def in_english(number) when number < 0 or number > 999_999_999_999 do
-    {:error, "number is out of range"}
-  end
+  @spec in_english(integer) :: {:ok | :error, String.t()}
+  def in_english(number) when number < 0 or number > 999_999_999_999,
+    do: {:error, "number is out of range"}
 
-  def in_english(n) do
-    {:ok,
-      cond do
-        n < 100           -> small(n)
-        n < 1000          -> compound(n, "hundred", 100)
-        n < 1_000_000     -> compound(n, "thousand", 1000)
-        n < 1_000_000_000 -> compound(n, "million", 1_000_000)
-        true              -> compound(n, "billion", 1_000_000_000)
-      end
-    }
-  end
+  def in_english(n), do: {:ok, say(n)}
+
+  defp divmod(number, divisor), do: {div(number, divisor), rem(number, divisor)}
+
+  defp say(n) when n < 100,           do: small(n)
+  defp say(n) when n < 1000,          do: compound(divmod(n, 100), "hundred")
+  defp say(n) when n < 1_000_000,     do: compound(divmod(n, 1000), "thousand")
+  defp say(n) when n < 1_000_000_000, do: compound(divmod(n, 1_000_000), "million")
+  defp say(n),                        do: compound(divmod(n, 1_000_000_000), "billion")
 
   defp small(n) when n < 20, do: elem(@ones, n)
-
-  defp small(n), do: do_small(divmod(n, 10))
-
-  defp do_small({div, 0}) do
-    elem(@tens, div)
+  defp small(n) do
+    case divmod(n, 10) do
+      {tens,    0} -> elem(@tens, tens)
+      {tens, ones} -> "#{elem(@tens, tens)}-#{elem(@ones, ones)}"
+    end
   end
 
-  defp do_small({div, rem}) do 
-    elem(@tens, div) <> "-" <> elem(@ones, rem)
-  end
-
-  defp compound(n, unit, base), do: do_compound(divmod(n, base), unit)
-
-  defp do_compound({div, 0}, unit) do
-    "#{elem(in_english(div), 1)} #{unit}"
-  end
-
-  defp do_compound({div, rem}, unit) do
-    "#{elem(in_english(div), 1)} #{unit} #{elem(in_english(rem), 1)}"
-  end
-
-  defp divmod(num, div), do: {div(num, div), rem(num, div)}
+  defp compound({quo,   0}, unit), do: "#{say(quo)} #{unit}"
+  defp compound({quo, rem}, unit), do: "#{say(quo)} #{unit} #{say(rem)}"
 end
