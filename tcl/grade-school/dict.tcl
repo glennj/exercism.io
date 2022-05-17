3da97c0e4dc7d7be45f9f3b6fe2464cd2a2d5514
Glenn Jackman <jackman@pythian.com>
2022-05-16 21:02:49 -0400

    tcl: refresh all

--- tcl/grade-school/dict.tcl
+++ tcl/grade-school/dict.tcl
@@ -3,18 +3,31 @@
 #    dict getdef dictionaryValue ?key ...? key default
 #
 # This will be in Tcl 8.7 -- https://tcl.tk/man/tcl8.7/TclCmd/dict.htm
-#
-proc ::tcl::dict::getdef {dictValue args} {
-    # `set` is a command in the ::tcl::dict namespace, so qualify it
-    ::set default [lindex $args end]
-    ::set keys [lrange $args 0 end-1]
-    if {[dict exists $dictValue {*}$keys]} {
-        return [dict get $dictValue {*}$keys]
-    } else {
-        return $default
-    }
-}
-namespace ensemble configure dict -map [concat \
-    [namespace ensemble configure dict -map] \
-    getdef ::tcl::dict::getdef ]
 
+package provide dictGetdef 0.1
+
+set dictSubcommands [namespace ensemble configure dict -map]
+
+if {![dict exists $dictSubcommands getdef]} {
+    proc ::tcl::dict::getdef {dictValue args} {
+        if {[llength $args] < 2} {
+            error {wrong # args: should be "dict getdef dictionary ?key ...? key default"}
+        }
+
+        # `set` is a command in the ::tcl::dict namespace, 
+        # so this usage needs to be fully qualified
+        ::set default [lindex $args end]
+        ::set keys [lrange $args 0 end-1]
+
+        if {[dict exists $dictValue {*}$keys]} {
+            return [dict get $dictValue {*}$keys]
+        } else {
+            return $default
+        }
+    }
+
+    dict set dictSubcommands getdef ::tcl::dict::getdef
+    namespace ensemble configure dict -map $dictSubcommands
+}
+
+unset dictSubcommands
--- tcl/robot-name/robot-name.tcl
+++ tcl/robot-name/robot-name.tcl
@@ -1,115 +1,65 @@
-package require itcl
-
-# incr Tcl has 
-# * class variables => "common"
-# * class methods   => "proc"
-# * access protection => public/protected/private
-
-itcl::class iRobot {
-    private {
-        common allNames {}
-        common nextNameIndex 0
-        variable name
-    }
+oo::class create Robot {
+    variable name
 
     constructor {} {
-        # the list of all names is created when the first 
-        # object is instantiated
-        if {[llength $allNames] == 0} {
-            generateNames
-        }
-        set name [nextName]
+        my reset
     }
 
-    method name {} {
-        return $name
-    }
+    method reset {} {set name [RobotNames::nextName]}
+    method name {} {return $name}
+}
 
-    method reset {} {
-        set name [nextName]
-    }
+# using a namespace to hold the global list of available robot names.  
+namespace eval RobotNames {
+    namespace export generate nextName
 
-    # The static methods
-    #
-    private proc generateNames {} {
-        # generate all possible names
-        set start [clock milliseconds]
-        foreach a {A B C D E F G H I J K L M N O P Q R S T U V W X Y Z} {
-            foreach b {A B C D E F G H I J K L M N O P Q R S T U V W X Y Z} {
+    proc generate {} {
+        variable names
+        variable index
+        variable count
+
+        set names {}
+        set index 0
+        set alphabet {A B C D E F G H I J K L M N O P Q R S T U V W X Y Z}
+
+        foreach a $alphabet {
+            foreach b $alphabet {
                 for {set c 0} {$c < 1000} {incr c} {
-                    lappend allNames [format "%s%s%03d" $a $b $c]
+                    lappend names [format {%s%s%03d} $a $b $c]
                 }
             }
         }
-        # shuffle the list
-        for {set idx1 [expr {[llength $allNames] - 1}]} {$idx1 > 0} {incr idx1 -1} {
-            set idx2 [expr {int(($idx1 + 1) * rand())}]
-            set temp [lindex $allNames $idx1]
-            lset allNames $idx1 [lindex $allNames $idx2]
-            lset allNames $idx2 $temp
+        # shuffle names
+        set count [llength $names]
+        return
+    }
+
+    proc nextName {} {
+        variable names
+        variable index
+        variable count
+
+        if {$index >= $count} {
+            error "all names in use"
         }
-    }
-
-    private proc nextName {} {
-        if {$nextNameIndex >= [llength $allNames]} {
-            error "All robot names have been used."
-        }
-        set n [lindex $allNames $nextNameIndex]
-        incr nextNameIndex
-        return $n
-    }
-
-    proc resetNames {} {
-        set nextNameIndex 0
+        set name [lindex $names $index]
+        incr index
+        return $name
     }
 }
 
+proc resetRobotNames {} {RobotNames::generate}
 
-# incr Tcl objects are not instantiated with the "new" command,
-# so we need a TclOO wrapper class
-oo::class create Robot {
-    variable robot
-    constructor {} {
-        set robot [iRobot #auto]
+# ref: https://wiki.tcl-lang.org/page/Shuffle+a+list
+proc shuffle {listvar} {
+    upvar 1 $listvar list
+    set n [llength $list]
+    for {set i [expr {$n - 1}]} {$i > 0} {incr i -1} {
+        set j [expr {int(rand() * ($i + 1))}]
+        set temp [lindex $list $i]
+        lset list $i [lindex $list $j]
+        lset list $j $temp
     }
-    method robot {cmd} {return [$robot $cmd]}
-    unexport robot
-    forward name    my robot name
-    forward reset   my robot reset
+    return
 }
 
-proc resetRobotNames {} {
-    iRobot::resetNames
-}
-
-
-############################################################
-# benchmarking:
-#
-# 1. using the incr Tcl class
-#
-#    ++++ robot-1 took 830 ms
-#    ++++ robot-2 took 0 ms
-#    ++++ robot-3 took 0 ms
-#    ++++ robot-4 took 0 ms
-#    ++++ robot-5 took 0 ms
-#    ++++ robot-6 took 0 ms
-#    ++++ robot-7 took 0 ms
-#    ++++ robot-8 took 0 ms
-#    ++++ robot-9 took 2727 ms
-#    ++++ robot-10 took 19552 ms
-#    robot-name.test:	Total	10	Passed	10	Skipped	0	Failed	0
-#
-# 2. using the reference solution (first ~830 ms happens before first test)
-#
-#    ++++ robot-1 took 0 ms
-#    ++++ robot-2 took 0 ms
-#    ++++ robot-3 took 0 ms
-#    ++++ robot-4 took 0 ms
-#    ++++ robot-5 took 0 ms
-#    ++++ robot-6 took 0 ms
-#    ++++ robot-7 took 0 ms
-#    ++++ robot-8 took 0 ms
-#    ++++ robot-9 took 595 ms
-#    ++++ robot-10 took 4464 ms
-#    robot-name.test:	Total	10	Passed	10	Skipped	0	Failed	0
