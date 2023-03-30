bd82d97c3cc9b1f0da34e0449391621d8c1a2112
Glenn Jackman <glenn.jackman@gmail.com>
2023-03-30 17:59:53 +0000

    go

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -1,38 +1,60 @@
 package account
 
+import "sync"
+
 type Account struct {
 	balance int64
-	open    bool
+	isOpen  bool
+	mutex   sync.Mutex
 }
 
-func Open(amount int64) *Account {
-	if amount < 0 {
-		return nil
+func Open(amount int64) (a *Account) {
+	if amount >= 0 {
+		a = &Account{balance: amount, isOpen: true}
 	}
-	return &Account{balance: amount, open: true}
+	return
 }
 
-func (a *Account) Balance() (int64, bool) {
-	if !a.open {
-		return 0, false
+func (a *Account) Balance() (balance int64, ok bool) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	if a.isOpen {
+		balance = a.balance
+		ok = true
 	}
-	return a.balance, true
+	return
 }
 
-func (a *Account) Deposit(amount int64) (int64, bool) {
-	if !a.open || amount+a.balance < 0 {
-		return 0, false
+func (a *Account) Deposit(amount int64) (balance int64, ok bool) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	if a.isOpen && amount+a.balance >= 0 {
+		a.balance += amount
+		balance = a.balance
+		ok = true
 	}
-	a.balance += amount
-	return a.balance, true
+	return
 }
 
-func (a *Account) Close() (int64, bool) {
-	if !a.open {
-		return 0, false
+func (a *Account) Close() (cash int64, ok bool) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
+	if a.isOpen {
+		cash = a.balance
+		a.balance = 0
+		a.isOpen = false
+		ok = true
 	}
-	cash := a.balance
-	a.balance = 0
-	a.open = false
-	return cash, true
+	return
 }
+
+/* benches
+ *
+ * on a 1 core linode:
+ * BenchmarkAccountOperations              34020756        34.05 ns/op       0 B/op       0 allocs/op
+ * BenchmarkAccountOperationsParallel      36511257        35.55 ns/op       0 B/op       0 allocs/op
+ *
+ * on my laptop
+ * BenchmarkAccountOperations-12           38972752        31.25 ns/op       0 B/op       0 allocs/op
+ * BenchmarkAccountOperationsParallel-12   11818558       102.9 ns/op        0 B/op       0 allocs/op
+ */
