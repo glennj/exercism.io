3fefc147ab65b0aa4a9bccc3c8fa086b516b299f
Glenn Jackman <glenn.jackman@gmail.com>
2023-04-20 21:49:28 +0000

    inspiration for bash scrabble exercise

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -47,5 +47,27 @@ using_case_glob() {
     echo $sum
 }
 
+# Mapping the letters to their score by using a sparse array
+# and iterating over the indices.
+# This one will be somewhat less efficient due to the nested loops
+# but I would expect it to be roughly the same as the `case` solution.
+using_array() {
+    shopt -s nocasematch
+
+    local groups=( [1]=AEIOULNRST [2]=DG [3]=BCMP [4]=FHVWY [5]=K [8]=JX [10]=QZ )
+    local sum=0 char
+
+    while IFS= read -d '' -n1 -r char; do
+        for score in "${!groups[@]}"; do
+            if [[ ${groups[score]} == *"$char"* ]]; then
+                ((sum += score))
+                break
+            fi
+        done
+    done < <(printf '%s' "$1")
+    echo $sum
+}
+
 #using_associative_array "$@"
-using_case_glob "$@"
+#using_case_glob "$@"
+using_array "$@"
