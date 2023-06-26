ac4ff0f3517c0356a1f628427394771c3cbbcfd7
Glenn Jackman <glenn.jackman@gmail.com>
2023-06-26 18:29:44 -0400

    clojure

---
+++ clojure/nucleotide-count/src/nucleotide_count.clj
@@ -0,0 +1,22 @@
+(ns nucleotide-count)
+
+(def init-nucleotide-count {\A 0 \C 0 \G 0 \T 0})
+
+(defn validate-nucleotide [nucleotide]
+  (when-not (find init-nucleotide-count nucleotide)
+    (throw (IllegalArgumentException. "invalid nucleotide"))))
+
+(defn validate-strand [strand]
+  (loop [[h & t] strand]
+    (when h
+      (validate-nucleotide h)
+      (recur t))))
+
+(defn nucleotide-counts [strand]
+  (validate-strand strand)
+  (->> (frequencies strand)
+       (merge init-nucleotide-count)))
+
+(defn count-of-nucleotide-in-strand [nucleotide strand]
+  (validate-nucleotide nucleotide)
+  (get (nucleotide-counts strand) nucleotide))
