cf33479fe5d95e28cf6ec3059ad4290d4a628bf5
Glenn Jackman <jackman@pythian.com>
2023-09-25 16:06:54 -0400

    nim linked-list

--- nim/linked-list/HELP.md
+++ nim/linked-list/HELP.md
@@ -25,6 +25,7 @@ It's possible to submit an incomplete solution which allows you to:
 If you'd like help solving the exercise, check the following pages:
 
 - The [Nim track's documentation](https://exercism.org/docs/tracks/nim)
+- The [Nim track's programming category on the forum](https://forum.exercism.org/c/programming/nim)
 - [Exercism's programming category on the forum](https://forum.exercism.org/c/programming/5)
 - The [Frequently Asked Questions](https://exercism.org/docs/using/faqs)
 
--- nim/linked-list/linked_list.nim
+++ nim/linked-list/linked_list.nim
@@ -1,27 +1,97 @@
-type
-  LinkedList*[T] = object ## A doubly linked list.
+import std/options
+import std/sugar
 
-proc len*[T](list: LinkedList[T]): int =
-  ## Returns the number of nodes in `list`.
-  var n = 0
-  discard
+#[  This is LinkedList implemented as a node.
+    To find the last node (and the length), I need to walk the list.
+    This is slower, but it's an experiment.
+
+    I've also implemented a `forEach` method.
+    I use that to find the length, the last node, and for deleting.
+]#
+
+type
+  LinkedList*[T] = ref object
+    next, prev: LinkedList[T]
+    value: Option[T]
+
+# Utility methods
+
+proc isEmpty[T](list: LinkedList[T]): bool = list.value.isNone
+proc hasNext[T](list: LinkedList[T]): bool = list.next != nil
+proc hasPrev[T](list: LinkedList[T]): bool = list.prev != nil
+
+proc lastNode[T](list: LinkedList[T]): LinkedList[T] =
+  var last = list
+  list.forEach((node: LinkedList[T]) => (last = node))
+  return last
+
+# Adding values to the list
 
 proc push*[T](list: var LinkedList[T], val: T) =
-  ## Appends a node with the given `val` to `list`.
-  discard
-
-proc pop*[T](list: var LinkedList[T]): T =
-  ## Removes the final node of `list` and returns its value.
-  discard
-
-proc shift*[T](list: var LinkedList[T]): T =
-  ## Removes the first node of `list` and returns its value.
-  discard
+  if list.isEmpty:
+    list.value = some(val)
+  else:
+    var last = list.lastNode
+    var newList = LinkedList[T]()
+    last.next = newList
+    newList.prev = last
+    newList.value = some(val)
 
 proc unshift*[T](list: var LinkedList[T], val: T) =
-  ## Prepends a node with the given `val` to `list`.
-  discard
+  if list.isEmpty:
+    list.value = some(val)
+  else:
+    var newList = LinkedList[T]()
+    newList.value = some(val)
+    newList.next = list
+    list.prev = newList
+    list = newList
+
+# Extracting values from the list
+
+proc pop*[T](list: var LinkedList[T]): T =
+  var node = list.lastNode
+  result = node.value.get
+  if node.hasPrev:
+    node.prev.next = nil
+    node.prev = nil
+  else:
+    node.value = none(T)
+
+proc shift*[T](list: var LinkedList[T]): T =
+  result = list.value.get
+  if list.hasNext:
+    list.next.prev = nil
+    list = list.next
+  else:
+    list.value = none(T)
+
+# Deleting values from the list
 
 proc delete*[T](list: var LinkedList[T], val: T) =
-  ## Removes a node with value `val` from `list`.
-  discard
+  var foundIt = false
+  var newList = LinkedList[T]()
+  list.forEach((node: LinkedList[T]) => (
+    if node.value == some(val) and not foundIt:
+      foundIt = true
+    else:
+      newList.push(node.value.get)
+  ))
+  list = newList
+
+# List size
+
+proc len*[T](list: LinkedList[T]): int =
+  var count = 0
+  list.forEach((node: LinkedList[T]) => (count = count + 1))
+  return count
+
+# Iterating over the list
+
+proc forEach*[T](list: LinkedList[T], callback: (lst: LinkedList[T]) -> void) =
+  if list.isEmpty: return
+  var node = list
+  while true:
+    callback(node)
+    if not node.hasNext: break
+    node = node.next
