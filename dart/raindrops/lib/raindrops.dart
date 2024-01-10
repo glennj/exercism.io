c5d8851773e9ea0535955bc849181bc743020dc2
Glenn Jackman <glenn.jackman@gmail.com>
2024-01-10 17:50:10 -0500

    dart format and lint raindrops

--- dart/raindrops/lib/raindrops.dart
+++ dart/raindrops/lib/raindrops.dart
@@ -1,9 +1,9 @@
 class Raindrops {
   String convert(int num) {
     var drops = StringBuffer();
-    if (num % 3 == 0)  drops.write("Pling");
-    if (num % 5 == 0)  drops.write("Plang");
-    if (num % 7 == 0)  drops.write("Plong");
+    if (num % 3 == 0) drops.write('Pling');
+    if (num % 5 == 0) drops.write('Plang');
+    if (num % 7 == 0) drops.write('Plong');
     if (drops.isEmpty) drops.write(num);
     return drops.toString();
   }
