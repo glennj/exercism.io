bd7a4643c6fc610b4941e24c2bac67cd63500d5a
Glenn Jackman <glenn.jackman@gmail.com>
2024-01-10 17:03:31 -0500

    dart format and lint secret-handshake

--- dart/secret-handshake/lib/secret_handshake.dart
+++ dart/secret-handshake/lib/secret_handshake.dart
@@ -3,18 +3,21 @@ extension on int {
   bool isBitSetAt(int offset) => bitAt(offset) == 1;
 }
 
-const ACTIONS = <String>['wink', 'double blink', 'close your eyes', 'jump'];
+const actions = <String>['wink', 'double blink', 'close your eyes', 'jump'];
 
 class SecretHandshake {
   List<String> commands(int code) {
     var cmds = <String>[];
 
-    for (var i = 0; i < ACTIONS.length; i++)
-      if (code.isBitSetAt(i))
-        cmds.add(ACTIONS[i]);
+    for (var i = 0; i < actions.length; i++) {
+      if (code.isBitSetAt(i)) {
+        cmds.add(actions[i]);
+      }
+    }
 
-    if (code.isBitSetAt(ACTIONS.length))
+    if (code.isBitSetAt(actions.length)) {
       cmds = cmds.reversed.toList();
+    }
 
     return cmds;
   }
--- dart/secret-handshake/test/secret_handshake_test.dart
+++ dart/secret-handshake/test/secret_handshake_test.dart
@@ -13,51 +13,51 @@ void main() {
     test('double blink for 10', () {
       final result = secretHandshake.commands(2);
       expect(result, equals(<String>['double blink']));
-    }, skip: false);
+    }, skip: true);
 
     test('close your eyes for 100', () {
       final result = secretHandshake.commands(4);
       expect(result, equals(<String>['close your eyes']));
-    }, skip: false);
+    }, skip: true);
 
     test('jump for 1000', () {
       final result = secretHandshake.commands(8);
       expect(result, equals(<String>['jump']));
-    }, skip: false);
+    }, skip: true);
 
     test('combine two actions', () {
       final result = secretHandshake.commands(3);
       expect(result, equals(<String>['wink', 'double blink']));
-    }, skip: false);
+    }, skip: true);
 
     test('reverse two actions', () {
       final result = secretHandshake.commands(19);
       expect(result, equals(<String>['double blink', 'wink']));
-    }, skip: false);
+    }, skip: true);
 
     test('reversing one action gives the same action', () {
       final result = secretHandshake.commands(24);
       expect(result, equals(<String>['jump']));
-    }, skip: false);
+    }, skip: true);
 
     test('reversing no actions still gives no actions', () {
       final result = secretHandshake.commands(16);
       expect(result, equals(<String>[]));
-    }, skip: false);
+    }, skip: true);
 
     test('all possible actions', () {
       final result = secretHandshake.commands(15);
       expect(result, equals(<String>['wink', 'double blink', 'close your eyes', 'jump']));
-    }, skip: false);
+    }, skip: true);
 
     test('reverse all possible actions', () {
       final result = secretHandshake.commands(31);
       expect(result, equals(<String>['jump', 'close your eyes', 'double blink', 'wink']));
-    }, skip: false);
+    }, skip: true);
 
     test('do nothing for zero', () {
       final result = secretHandshake.commands(0);
       expect(result, equals(<String>[]));
-    }, skip: false);
+    }, skip: true);
   });
 }
