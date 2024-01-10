d7cebeb4be6d77d21c693c8ca6e922dabc45a864
Glenn Jackman <glenn.jackman@gmail.com>
2024-01-10 17:53:15 -0500

    dart format and lint pig-latin

--- dart/pig-latin/lib/pig_latin.dart
+++ dart/pig-latin/lib/pig_latin.dart
@@ -1,23 +1,24 @@
 String translate(String phrase) =>
-  phrase.replaceAllMapped(RegExp(r'\S+'), translateMatch);
+    phrase.replaceAllMapped(RegExp(r'\S+'), translateMatch);
 
 /* These regexes all capture a leading prefix and the rest of the string.
  * The pig-latin-ized translation is "{rest}{prefix}ay"
  */
-final Q = RegExp(r'^([^aeiou]?qu)(.*)');
-final Y = RegExp(r'^([^aeiou]+)(y.*)');
-final CONSONANTS = RegExp(r'^([^aeiou]+)(.*)');
+final q = RegExp(r'^([^aeiou]?qu)(.*)');
+final y = RegExp(r'^([^aeiou]+)(y.*)');
+final consonants = RegExp(r'^([^aeiou]+)(.*)');
 // for this one, the prefix is empty
-final VOWEL = RegExp(r'^()((?:[aeiou]|xr|yt).*)');
+final vowel = RegExp(r'^()((?:[aeiou]|xr|yt).*)');
 
-final RE = [VOWEL, Q, Y, CONSONANTS];   // use this order
+final re = [vowel, q, y, consonants]; // use this order
 
 String translateMatch(Match match) {
   var word = match[0]!;
-  for (var re in RE) {
-    var m = re.firstMatch(word);
-    if (m != null)
+  for (var r in re) {
+    var m = r.firstMatch(word);
+    if (m != null) {
       return '${m[2]!}${m[1]!}ay';
+    }
   }
 
   // are there other cases?
--- dart/pig-latin/test/pig_latin_test.dart
+++ dart/pig-latin/test/pig_latin_test.dart
@@ -12,117 +12,117 @@ void main() {
       test('word beginning with e', () {
         final result = pigLatin.translate('ear');
         expect(result, equals('earay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with i', () {
         final result = pigLatin.translate('igloo');
         expect(result, equals('iglooay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with o', () {
         final result = pigLatin.translate('object');
         expect(result, equals('objectay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with u', () {
         final result = pigLatin.translate('under');
         expect(result, equals('underay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with a vowel and followed by a qu', () {
         final result = pigLatin.translate('equal');
         expect(result, equals('equalay'));
-      }, skip: false);
+      }, skip: true);
     });
 
     group('first letter and ay are moved to the end of words that start with consonants', () {
       test('word beginning with p', () {
         final result = pigLatin.translate('pig');
         expect(result, equals('igpay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with k', () {
         final result = pigLatin.translate('koala');
         expect(result, equals('oalakay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with x', () {
         final result = pigLatin.translate('xenon');
         expect(result, equals('enonxay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with q without a following u', () {
         final result = pigLatin.translate('qat');
         expect(result, equals('atqay'));
-      }, skip: false);
+      }, skip: true);
     });
 
     group('some letter clusters are treated like a single consonant', () {
       test('word beginning with ch', () {
         final result = pigLatin.translate('chair');
         expect(result, equals('airchay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with qu', () {
         final result = pigLatin.translate('queen');
         expect(result, equals('eenquay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with qu and a preceding consonant', () {
         final result = pigLatin.translate('square');
         expect(result, equals('aresquay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with th', () {
         final result = pigLatin.translate('therapy');
         expect(result, equals('erapythay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with thr', () {
         final result = pigLatin.translate('thrush');
         expect(result, equals('ushthray'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with sch', () {
         final result = pigLatin.translate('school');
         expect(result, equals('oolschay'));
-      }, skip: false);
+      }, skip: true);
     });
 
     group('some letter clusters are treated like a single vowel', () {
       test('word beginning with yt', () {
         final result = pigLatin.translate('yttria');
         expect(result, equals('yttriaay'));
-      }, skip: false);
+      }, skip: true);
 
       test('word beginning with xr', () {
         final result = pigLatin.translate('xray');
         expect(result, equals('xrayay'));
-      }, skip: false);
+      }, skip: true);
     });
 
     group('position of y in a word determines if it is a consonant or a vowel', () {
       test('y is treated like a consonant at the beginning of a word', () {
         final result = pigLatin.translate('yellow');
         expect(result, equals('ellowyay'));
-      }, skip: false);
+      }, skip: true);
 
       test('y is treated like a vowel at the end of a consonant cluster', () {
         final result = pigLatin.translate('rhythm');
         expect(result, equals('ythmrhay'));
-      }, skip: false);
+      }, skip: true);
 
       test('y as second letter in two letter word', () {
         final result = pigLatin.translate('my');
         expect(result, equals('ymay'));
-      }, skip: false);
+      }, skip: true);
     });
 
     group('phrases are translated', () {
       test('a whole phrase', () {
         final result = pigLatin.translate('quick fast run');
         expect(result, equals('ickquay astfay unray'));
-      }, skip: false);
+      }, skip: true);
     });
   });
 }
