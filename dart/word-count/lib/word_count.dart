fe0eafceb44d0b24d42fd89bcc6025c39cea4655
Glenn Jackman <glenn.jackman@gmail.com>
2024-01-10 16:28:26 -0500

    dart format word-count

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -3,16 +3,17 @@ extension on String {
     var re = '(?:^${char}+)|(?:${char}+\$)';
     return this.replaceAll(RegExp(re), '');
   }
+
+  String trimSingleQuotes() => trimChar("'");
 }
 
+Map<String, int> incrCount(Map<String, int> count, String word) =>
+    count..update(word, (c) => c + 1, ifAbsent: () => 1);
+
 class WordCount {
-  Map<String, int> countWords(String input) =>
-      RegExp(r"[\w']+")
-        .allMatches(input.toLowerCase())
-        .map((match) => match[0]!.trimChar("'"))
-        .where((word) => word.isNotEmpty)
-        .fold({}, (count, word) {
-          count[word] = 1 + (count[word] ?? 0);
-          return count;
-        });
+  Map<String, int> countWords(String input) => RegExp(r"[\w']+")
+      .allMatches(input.toLowerCase())
+      .map((match) => match[0]!.trimSingleQuotes())
+      .where((word) => word.isNotEmpty)
+      .fold({}, incrCount);
 }
--- dart/word-count/test/word_count_test.dart
+++ dart/word-count/test/word_count_test.dart
@@ -13,37 +13,37 @@ void main() {
     test('count one of each word', () {
       final result = wordCount.countWords('one of each');
       expect(result, equals(<String, int>{'one': 1, 'of': 1, 'each': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('multiple occurrences of a word', () {
       final result = wordCount.countWords('one fish two fish red fish blue fish');
       expect(result, equals(<String, int>{'one': 1, 'fish': 4, 'two': 1, 'red': 1, 'blue': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('handles cramped lists', () {
       final result = wordCount.countWords('one,two,three');
       expect(result, equals(<String, int>{'one': 1, 'two': 1, 'three': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('handles expanded lists', () {
       final result = wordCount.countWords('one,\ntwo,\nthree');
       expect(result, equals(<String, int>{'one': 1, 'two': 1, 'three': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('ignore punctuation', () {
       final result = wordCount.countWords('car: carpet as java: javascript!!&@\$%^&');
       expect(result, equals(<String, int>{'car': 1, 'carpet': 1, 'as': 1, 'java': 1, 'javascript': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('include numbers', () {
       final result = wordCount.countWords('testing, 1, 2 testing');
       expect(result, equals(<String, int>{'testing': 2, '1': 1, '2': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('normalize case', () {
       final result = wordCount.countWords('go Go GO Stop stop');
       expect(result, equals(<String, int>{'go': 3, 'stop': 2}));
-    }, skip: false);
+    }, skip: true);
 
     test('with apostrophes', () {
       final result = wordCount.countWords('\'First: don\'t laugh. Then: don\'t cry. You\'re getting it.\'');
@@ -59,12 +59,12 @@ void main() {
             'getting': 1,
             'it': 1
           }));
-    }, skip: false);
+    }, skip: true);
 
     test('with quotations', () {
       final result = wordCount.countWords('Joe can\'t tell between \'large\' and large.');
       expect(result, equals(<String, int>{'joe': 1, 'can\'t': 1, 'tell': 1, 'between': 1, 'large': 2, 'and': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('substrings from the beginning', () {
       final result = wordCount.countWords('Joe can\'t tell between app, apple and a.');
@@ -72,21 +72,21 @@ void main() {
           result,
           equals(
               <String, int>{'joe': 1, 'can\'t': 1, 'tell': 1, 'between': 1, 'app': 1, 'apple': 1, 'and': 1, 'a': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('multiple spaces not detected as a word', () {
       final result = wordCount.countWords(' multiple   whitespaces');
       expect(result, equals(<String, int>{'multiple': 1, 'whitespaces': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('alternating word separators not detected as a word', () {
       final result = wordCount.countWords(',\n,one,\n ,two \n \'three\'');
       expect(result, equals(<String, int>{'one': 1, 'two': 1, 'three': 1}));
-    }, skip: false);
+    }, skip: true);
 
     test('quotation for word with apostrophe', () {
       final result = wordCount.countWords('can, can\'t, \'can\'t\'');
       expect(result, equals(<String, int>{'can': 1, 'can\'t': 2}));
-    }, skip: false);
+    }, skip: true);
   });
 }
