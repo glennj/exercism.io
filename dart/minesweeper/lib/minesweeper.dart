95ac3b5321e389492478672db3666450c56b399e
Glenn Jackman <jackman@pythian.com>
2024-01-12 17:22:28 -0500

    dart

---
+++ dart/minesweeper/lib/minesweeper.dart
@@ -0,0 +1,45 @@
+class Minesweeper {
+  final List<String> board;
+  Minesweeper(this.board);
+
+  final _annotated = <String>[];
+
+  List<String> get annotated {
+    if (_annotated.isEmpty) _annotate();
+    return _annotated;
+  }
+
+  void _annotate() {
+    var annotated = board
+        .map((row) => row.split('').map((c) => c == '*' ? 99 : 0).toList())
+        .toList();
+
+    bool onBoard(int x, int y) =>
+        (0 <= y && y < board.length) && (0 <= x && x < board[y].length);
+
+    void incrNeighbours(int x, int y) {
+      for (var dy in [-1, 0, 1]) {
+        var yy = y + dy;
+        for (var dx in [-1, 0, 1]) {
+          var xx = x + dx;
+          if (onBoard(xx, yy) && annotated[yy][xx] != 99) {
+            annotated[yy][xx]++;
+          }
+        }
+      }
+    }
+
+    for (var y = 0; y < annotated.length; y++) {
+      for (var x = 0; x < annotated[y].length; x++) {
+        if (annotated[y][x] == 99) incrNeighbours(x, y);
+      }
+    }
+
+    for (var row in annotated) {
+      var str = row
+          .map((n) => switch (n) { 0 => ' ', 99 => '*', _ => n.toString() })
+          .join('');
+      _annotated.add(str);
+    }
+  }
+}
---
+++ dart/minesweeper/test/minesweeper_test.dart
@@ -0,0 +1,66 @@
+import 'package:minesweeper/minesweeper.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Minesweeper', () {
+    test('no rows', () {
+      final result = Minesweeper(<String>[]).annotated;
+      expect(result, equals(<String>[]));
+    }, skip: false);
+
+    test('no columns', () {
+      final result = Minesweeper(<String>['']).annotated;
+      expect(result, equals(<String>['']));
+    }, skip: false);
+
+    test('no mines', () {
+      final result = Minesweeper(<String>['   ', '   ', '   ']).annotated;
+      expect(result, equals(<String>['   ', '   ', '   ']));
+    }, skip: false);
+
+    test('minefield with only mines', () {
+      final result = Minesweeper(<String>['***', '***', '***']).annotated;
+      expect(result, equals(<String>['***', '***', '***']));
+    }, skip: false);
+
+    test('mine surrounded by spaces', () {
+      final result = Minesweeper(<String>['   ', ' * ', '   ']).annotated;
+      expect(result, equals(<String>['111', '1*1', '111']));
+    }, skip: false);
+
+    test('space surrounded by mines', () {
+      final result = Minesweeper(<String>['***', '* *', '***']).annotated;
+      expect(result, equals(<String>['***', '*8*', '***']));
+    }, skip: true);
+
+    test('horizontal line', () {
+      final result = Minesweeper(<String>[' * * ']).annotated;
+      expect(result, equals(<String>['1*2*1']));
+    }, skip: true);
+
+    test('horizontal line, mines at edges', () {
+      final result = Minesweeper(<String>['*   *']).annotated;
+      expect(result, equals(<String>['*1 1*']));
+    }, skip: true);
+
+    test('vertical line', () {
+      final result = Minesweeper(<String>[' ', '*', ' ', '*', ' ']).annotated;
+      expect(result, equals(<String>['1', '*', '2', '*', '1']));
+    }, skip: true);
+
+    test('vertical line, mines at edges', () {
+      final result = Minesweeper(<String>['*', ' ', ' ', ' ', '*']).annotated;
+      expect(result, equals(<String>['*', '1', ' ', '1', '*']));
+    }, skip: true);
+
+    test('cross', () {
+      final result = Minesweeper(<String>['  *  ', '  *  ', '*****', '  *  ', '  *  ']).annotated;
+      expect(result, equals(<String>[' 2*2 ', '25*52', '*****', '25*52', ' 2*2 ']));
+    }, skip: true);
+
+    test('large minefield', () {
+      final result = Minesweeper(<String>[' *  * ', '  *   ', '    * ', '   * *', ' *  * ', '      ']).annotated;
+      expect(result, equals(<String>['1*22*1', '12*322', ' 123*2', '112*4*', '1*22*2', '111111']));
+    }, skip: true);
+  });
+}
