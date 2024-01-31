3458a6c14b3075fb773dc4b3c69887360fc2ad71
Glenn Jackman <jackman@pythian.com>
2024-01-31 12:20:14 -0500

    misc

--- c/leap/leap.c
+++ c/leap/leap.c
@@ -1,5 +1,35 @@
 #include "leap.h"
 
-bool leap_year(int year) {
+static bool approach_1(int year) {
     return year % 4 == 0 && (year % 100 != 0 || year % 400 == 0);
+}
+
+/*
+static bool approach_2(int year) {
+    if (year % 100 == 0) {
+        return year % 400 == 0;
+    } else {
+        return year % 4 == 0;
+    }
+}
+*/
+
+/* approach 1 uses 1 to 3 comparisions per call;
+ * approach 2 uses exactly 2 comparisons per call.
+ *
+ * Looking at the years 1 to 2000, how many total comparisons:
+ *
+ * divisible by 400 => 5
+ * divisible by 100 => 15
+ * divisible by   4 => 480
+ * otherwise        => 1500
+ *
+ * approach 1: 5 * 3 + 15 * 3 + 480 * 2 + 1500 * 1 == 2520
+ * approach 2: 2000 * 2 == 4000
+ *
+ * On the whole, approach 1 should be faster.
+ */
+
+bool leap_year(int year) {
+    return approach_1(year);
 }
