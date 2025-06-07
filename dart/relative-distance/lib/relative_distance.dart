867723a2d35871d62d64dcff1e862283c31366f4
Glenn Jackman <glenn.jackman@gmail.com>
2025-06-07 08:31:18 -0400

    dart

--- dart/relative-distance/lib/relative_distance.dart
+++ dart/relative-distance/lib/relative_distance.dart
@@ -9,23 +9,23 @@ class RelativeDistance {
     var pathA = _pathToRoot(a, []);
     var pathB = _pathToRoot(b, []);
 
-    if (pathA.isEmpty && pathB.isEmpty)
-      return -1; // unrelated
+    // unrelated cases
+    if (pathA.isEmpty && pathB.isEmpty) return -1;
+    if (pathA.isEmpty) return (a != pathB[0]) ? -1 : pathB.length;
+    if (pathB.isEmpty) return (b != pathA[0]) ? -1 : pathA.length;
+    if (pathA[0] != pathB[0]) return -1;
 
-    if (pathA.isEmpty)
-      return pathB.length;
-
-    if (pathB.isEmpty)
-      return pathA.length;
-
-    var paths = _removeCommonPrefix(pathA, pathB);
-    return paths.first.length + paths.last.length + 1;
+    // remove common prefix
+    while (!pathA.isEmpty && !pathB.isEmpty && pathA[0] == pathB[0]) {
+      pathA.removeAt(0);
+      pathB.removeAt(0);
+    }
+    return pathA.length + pathB.length + 1;
   }
-  
+
   Names _pathToRoot(Name name, Names path) {
     var parent = _findParent(name);
-    if (parent == null)
-      return path;
+    if (parent == null) return path;
 
     path.insert(0, parent);
     return _pathToRoot(parent, path);
@@ -33,17 +33,8 @@ class RelativeDistance {
 
   Name? _findParent(Name child) {
     for (var entry in _familyTree.entries) {
-      if (entry.value.contains(child))
-        return entry.key;
+      if (entry.value.contains(child)) return entry.key;
     }
     return null;
-  }
-
-  List<Names> _removeCommonPrefix(Names a, Names b) {
-    while (!a.isEmpty && !b.isEmpty && a[0] == b[0]) {
-      a.removeAt(0);
-      b.removeAt(0);
-    }
-    return [a, b];
   }
 }
