eb00b76e367533348337023a058496b0d99a1e86
Glenn Jackman <glenn.jackman@gmail.com>
2025-08-20 19:53:10 -0400

    x

--- tcl/relative-distance/relative-distance.tcl
+++ tcl/relative-distance/relative-distance.tcl
@@ -1,28 +1,48 @@
-source ./dict.tcl
-
 proc degreeOfSeparation {personA personB familyTree} {
     set neighbours [getNeighboursFromTree $familyTree]
-    puts [list $neighbours]
+    set queue      [list [list $personA 0]]
+    set visited    [dict create $personA true]
 
-    return 1
+    while {[llength $queue] > 0} {
+        set pair [lpop queue 0]
+        lassign $pair person degree
+
+        if {$person eq $personB} {
+            return $degree
+        }
+
+        incr degree
+        foreach neighbour [dict get $neighbours $person] {
+            if {[dict getdef $visited $neighbour false]} then continue
+            dict set visited $neighbour true
+            lappend queue [list $neighbour $degree]
+        }
+    }
+    return -1 ;# not related
 }
 
+############################################################
 proc getNeighboursFromTree {familyTree} {
     set neighbours [dict create]
 
     dict for {parent children} $familyTree {
         foreach child $children {
-            dict setIfAbsent neighbours $parent $child
-            dict setIfAbsent neighbours $child $parent
+            addIfAbsent neighbours $parent $child
+            addIfAbsent neighbours $child $parent
 
             foreach sibling $children {
-                if {$child ne $sibling} {
-                    dict setIfAbsent neighbours $child $sibling
-                    dict setIfAbsent neighbours $sibling $child
-                }
+                if {$child eq $sibling} then continue
+                addIfAbsent neighbours $child $sibling
+                addIfAbsent neighbours $sibling $child
             }
         }
     }
-
     return $neighbours
+}
+
+proc addIfAbsent {dictVarName key value} {
+    upvar 1 $dictVarName dictVar
+    if {[lsearch -exact [dict getdef $dictVar $key {}] $value] == -1} {
+        dict lappend dictVar $key $value
+    }
 }
