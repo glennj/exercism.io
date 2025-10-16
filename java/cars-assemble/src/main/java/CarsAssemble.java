c5114874bd7901636d6c889be5c08978e232a9f7
Glenn Jackman <glenn.jackman@gmail.com>
2025-10-16 08:44:32 -0400

    java

--- java/cars-assemble/src/main/java/CarsAssemble.java
+++ java/cars-assemble/src/main/java/CarsAssemble.java
@@ -1,18 +1,16 @@
 public class CarsAssemble {
-	private static final int CARS_PER_HOUR = 221;
-	private static final int MINUTES_PER_HOUR = 60;
+    private static final int CARS_PER_HOUR = 221;
+    private static final int MINUTES_PER_HOUR = 60;
+
+    private double successRate(int speed) {
+        if (1 <= speed && speed <= 4)  return 1.0;
+        if (5 <= speed && speed <= 8)  return 0.9;
+        if (speed == 9)                return 0.8;
+        return 0.77;
+    }
 
     public double productionRatePerHour(int speed) {
-		int rate = CARS_PER_HOUR * speed;
-
-        if (1 <= speed && speed <= 4)
-			return 1.0 * rate;
-		else if (5 <= speed && speed <= 8)
-			return 0.9 * rate;
-		else if (speed == 9)
-			return 0.8 * rate;
-		else
-			return 0.77 * rate;
+        return CARS_PER_HOUR * speed * successRate(speed);
     }
 
     public int workingItemsPerMinute(int speed) {
