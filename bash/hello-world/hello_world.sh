44340f332fbc82307c3b3cfaa203bc3197d9e508
Glenn Jackman <glenn.jackman@gmail.com>
2026-04-03 20:44:34 -0400

    refresh bash hello

--- bash/hello-world/hello_world.sh
+++ bash/hello-world/hello_world.sh
@@ -1,19 +1,4 @@
 #!/usr/bin/env bash
 
-if [[ ${BASH_VERSINFO[0]} -lt 4 ]]; then
 
-    # the sensible straightforward solution
-    echo "Hello, World!"
-
-else
-
-    # a goofy solution
-    command_not_found_handle() {
-        local -l cmd=$1; shift
-        local -l args=( "$@" )
-        printf '%s, %s!\n' "${cmd^}" "${args[*]^}"
-    }
-
-    hello WORLD
-
-fi
+echo "Hello, World!"
