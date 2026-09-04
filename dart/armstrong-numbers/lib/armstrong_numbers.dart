e9182c2244c27ad41992d22a62ba9230eb7d8050
Glenn Jackman <glenn.jackman@gmail.com>
2026-09-03 20:04:29 -0400

    revamp armstrong numbers in dart

--- dart/armstrong-numbers/lib/armstrong_numbers.dart
+++ dart/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,12 +1,9 @@
 class ArmstrongNumbers {
-  bool isArmstrongNumber(String number) {
-    var n = BigInt.parse(number);
-    var len = number.length;
-    var sum = number.runes
-                    .where((rune) => 48 <= rune && rune <= 57)   // ascii digits
-                    .map((rune) => BigInt.from(rune - 48))       // convert to numbers
-                    .fold(BigInt.zero, (sum, digit) => sum + digit.pow(len));
-    
-    return n == sum;
-  }
+  bool isArmstrongNumber(String number) => BigInt.parse(number) == armstrongSum(number);
+
+  BigInt armstrongSum(String number) =>
+      number.runes
+            .where((rune) => 48 <= rune && rune <= 57)   // ascii digits
+            .map((rune) => BigInt.from(rune - 48))       // convert to numbers
+            .fold(BigInt.zero, (sum, digit) => sum + digit.pow(number.length));
 }
