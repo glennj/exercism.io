b552814c6d17298273a29c53442319b82f271e53
Glenn Jackman <glenn.jackman@gmail.com>
2026-09-25 13:02:42 -0400

    freshen ruby zebra

--- ruby/zebra-puzzle/zebra_puzzle.rb
+++ ruby/zebra-puzzle/zebra_puzzle.rb
@@ -1,7 +1,24 @@
+# rubocop:disable Style/CommentedKeyword, Style/Documentation, Style/OneClassPerFile, Metrics
+# frozen_string_literal: true
+
 require 'singleton'
+
+module HousePredicates
+  refine Integer do
+    def right_of?(other)
+      self + 1 == other
+    end
+
+    def next_to?(other)
+      right_of?(other) || other.right_of?(self)
+    end
+  end
+end
 
 class ZebraPuzzle
   include Singleton
+
+  using HousePredicates
 
   def self.water_drinker
     instance.nationality_of(:water)
@@ -25,9 +42,11 @@ class ZebraPuzzle
 
   attr_reader :directory, :nationality
 
-  HOUSES = [1, 2, 3, 4, 5]                                  # clue 1
+  HOUSES = [1, 2, 3, 4, 5].freeze                       # clue 1
   FIRST = 1
   MIDDLE = 3
+
+  private_constant :HOUSES, :FIRST, :MIDDLE
 
   def initialize
     @directory = {}
@@ -35,65 +54,57 @@ class ZebraPuzzle
 
     # 1. colours
     HOUSES.permutation(HOUSES.size) do |(red, green, ivory, yellow, blue)|
-      if  right_of(green, ivory)                            # clue 6
+      next unless green.right_of?(ivory)                # clue 6
 
-        # 2. nationality
-        HOUSES.permutation(HOUSES.size) do |(gb, es, ua, no, jp)|
-          if  gb == red         &&                          # clue 2
-              no == FIRST       &&                          # clue 10
-              next_to(no, blue)                             # clue 15
+      # 2. nationality
+      HOUSES.permutation(HOUSES.size) do |(gb, es, ua, no, jp)|
+        next unless gb == red                           # clue 2
+        next unless no == FIRST                         # clue 10
+        next unless no.next_to?(blue)                   # clue 15
 
-            # 3. beverages
-            HOUSES.permutation(HOUSES.size) do |(coffee, tea, milk, oj, water)|
-              if  coffee == green  &&                       # clue 4
-                  tea == ua        &&                       # clue 5
-                  milk == MIDDLE                            # clue 9
+        # 3. beverages
+        HOUSES.permutation(HOUSES.size) do |(coffee, tea, milk, orange_juice, water)|
+          next unless coffee == green                   # clue 4
+          next unless tea == ua                         # clue 5
+          next unless milk == MIDDLE                    # clue 9
 
-                # 4. cigarettes
-                HOUSES.permutation(HOUSES.size) do |(oldgold, kools, chester, lucky, parl)|
-                  if  kools == yellow  &&                   # clue 8
-                      lucky == oj      &&                   # clue 13
-                      parl == jp                            # clue 14
+          # 4. cigarettes
+          HOUSES.permutation(HOUSES.size) do |(old_gold, kools, chesterfields, lucky_strikes, parliaments)|
+            next unless kools == yellow                 # clue 8
+            next unless lucky_strikes == orange_juice   # clue 13
+            next unless parliaments == jp               # clue 14
 
-                    # 5. pets
-                    HOUSES.permutation(HOUSES.size) do |(dog, snail, fox, horse, zebra)|
+            # 5. pets
+            HOUSES.permutation(HOUSES.size) do |(dog, snails, fox, horse, zebra)|
+              next unless dog == es                     # clue 3
+              next unless snails == old_gold            # clue 7
+              next unless fox.next_to?(chesterfields)   # clue 11
+              next unless horse.next_to?(kools)         # clue 12
 
-                      if  dog == es              &&         # clue 3
-                          snail == oldgold       &&         # clue 7
-                          next_to(fox, chester)  &&         # clue 11
-                          next_to(horse, kools)             # clue 12
+              # save the house number for each attribute
+              @directory = {
+                red:, green:, ivory:, yellow:, blue:,
+                english: gb, spanish: es, ukranian: ua, norwegian: no, japanese: jp,
+                coffee:, tea:, milk:, orange_juice:, water:,
+                old_gold:, kools:, chesterfields:, lucky_strikes:, parliaments:,
+                dog:, snails:, fox:, horse:, zebra:
+              }
 
-                        @directory = {
-                          red: red, green: green, ivory: ivory, yellow: yellow, blue: blue,
-                          english: gb, spanish: es, ukranian: ua, norwegian: no, japanese: jp,
-                          coffee: coffee, tea: tea, milk: milk, orange_juice: oj, water: water,
-                          old_gold: oldgold, kools: kools, chesterfields: chester, lucky_strikes: lucky, parliaments: parl,
-                          dog: dog, snails: snail, fox: fox, horse: horse, zebra: zebra,
-                        }
-                        @nationality[gb] = "English"
-                        @nationality[es] = "Spanish"
-                        @nationality[ua] = "Ukranian"
-                        @nationality[no] = "Norwegian"
-                        @nationality[jp] = "Japanese"
+              # save the nationality for each house number
+              @nationality[gb] = 'English'
+              @nationality[es] = 'Spanish'
+              @nationality[ua] = 'Ukranian'
+              @nationality[no] = 'Norwegian'
+              @nationality[jp] = 'Japanese'
 
-                        return
-                      end
-                    end
-                  end
-                end
-              end
-            end
-          end
-        end
-      end
-    end
-  end
-
-  def right_of(a, b)
-    a + 1 == b
-  end
-
-  def next_to(a, b)
-    right_of(a, b) || right_of(b, a)
+              # rubocop:disable-next Lint/NonLocalExitFromIterator
+              return
+            end # pets
+          end # cigarettes
+        end # beverages
+      end # nationality
+    end # colours
   end
 end
+
+# rubocop:enable Style/CommentedKeyword, Style/Documentation, Style/OneClassPerFile, Metrics
